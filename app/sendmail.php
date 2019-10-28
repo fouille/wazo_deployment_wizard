c735979b36694d8016f6900d82c302376ba07d84
Franck M <franckisys@gmail.com>
2019-10-28 17:36:56 +0100

    first commit

---
+++ app/sendmail.php
@@ -0,0 +1,28 @@
+<?php
+$tab = array();
+$mail = $_POST['email'];
+$to = "fmuller@wazo.io";/* YOUR EMAIL HERE */
+$subject = "Wizard Deployment Setup";
+$headers = "From: Wizard Deployment <fmuller@wazo.io>";
+$message = "DETAILS\n";
+$message .= "\nFirst name: " . $_POST['firstname'];
+$message .= "\nLast name: " . $_POST['lastname'];
+$message .= "\nEmail: " . $_POST['email'];
+$message .= "\nCountry: " . $_POST['function'];
+$message .= "\nSociety: " . $_POST['society'];
+
+//Receive Variable
+$sentOk = mail($to,$subject,$message,$headers);
+array_push($tab, $message);
+//Confirmation page
+$user = "$mail";
+$usersubject = "Merci !";
+$userheaders = "From: contact@wazo.io\n";
+/*$usermessage = "Thank you for your time. Your quotation request is successfully submitted.\n"; WITH OUT SUMMARY*/
+//Confirmation page WITH  SUMMARY
+$usermessage = "Thank you for your time. Your request is successfully submitted. We will reply shortly.\n\nBELOW A SUMMARY\n\n$message";
+mail($user,$usersubject,$usermessage,$userheaders);
+
+$json = array('sendmail' => $sentOk, 'mail' => $tab);
+echo json_encode($json);
+?>
