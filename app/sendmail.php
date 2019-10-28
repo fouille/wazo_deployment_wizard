4b84743c2708fa4be97a666b8e9efc8d56401c27
root <root@wazo.io>
2019-10-28 19:12:52 +0000

    debug

--- app/sendmail.php
+++ app/sendmail.php
@@ -1,29 +1,28 @@
 <?php
-$tab = array();
 $mail = $_POST['email'];
-$to = "franckisys@wazo.io";/* YOUR EMAIL HERE */
+
+$wazo = "fmuller@wazo.io\n";
 $subject = "Wizard Deployment Setup";
-$headers = "From: Wizard Deployment <fmuller@wazo.io>\n";
+$headers = "From: Wizard Deployment <pbx@wazo.io>\n";
 $message = "DETAILS\n";
 $message .= "\nFirst name: " . $_POST['firstname'];
 $message .= "\nLast name: " . $_POST['lastname'];
 $message .= "\nEmail: " . $_POST['email'];
-$message .= "\nCountry: " . $_POST['function'];
+$message .= "\nFonction: " . $_POST['fonction'];
 $message .= "\nSociety: " . $_POST['society'];
 
 //Receive Variable
-//$sentOk = mail($to,$subject,$message,$headers);
-mail($to,$subject,$message,$headers);
-array_push($tab, $message);
+
 //Confirmation page
 $user = "$mail";
 $usersubject = "Merci !";
-$userheaders = "From: pbx@wazo.io\n";
+$userheaders = "From: Wazo Deployment Wizard <pbx@wazo.io>\n";
 /*$usermessage = "Thank you for your time. Your quotation request is successfully submitted.\n"; WITH OUT SUMMARY*/
 //Confirmation page WITH  SUMMARY
 $usermessage = "Thank you for your time. Your request is successfully submitted. We will reply shortly.\n\nBELOW A SUMMARY\n\n$message";
-mail($user,$usersubject,$usermessage,$userheaders);
 
-$json = array('sendmail' => $sentOk, 'mail' => $tab);
-echo json_encode($json);
+if (mail($wazo,$subject,$message,$headers) == true) {
+mail($user,$usersubject,$usermessage,$userheaders);
+}
+
 ?>
